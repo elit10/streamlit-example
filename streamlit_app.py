0a45054adf00515c6f2642ea96eb0ccb2c60e81a
Onur Gök <32486897+elit10@users.noreply.github.com>
2024-05-08 11:02:05 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,15 @@ In the meantime, below is an example of what you can do with just a few lines of
 """
 
 "Deneme deneme böyle mi yazılıyor"
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+st.title('Multiplication App')
+
+num1 = st.number_input('Enter the first number', value=1, format='%d')
+num2 = st.number_input('Enter the second number', value=1, format='%d')
+
+result = multiply_numbers(num1, num2)
+
+st.write(f'The result of {num1} * {num2} is **{result}**')
