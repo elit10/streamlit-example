743d17887caa0ea70c468e1d2f00d35f46da8abd
Onur Gök <32486897+elit10@users.noreply.github.com>
2024-05-08 11:17:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,6 @@
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
+st.title("Deneme")
 "Deneme deneme böyle mi yazılıyor"
 
 def multiply_numbers(num1, num2):
